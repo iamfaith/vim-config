b95363fd38635f8ad4ddf11ac4e2318dc19ad533
xianzixiang <xianzixiang@gmail.com>
2018-09-18 20:07:21 +0800

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./vimrc ~/.vim/vimrc
-ln ~/.vimrc ~/.vim/vimrc
+ln -f ~/.vimrc ~/.vim/vimrc
 vim +PluginInstall +qall
 
 echo "dont forget to install / update YoucompleteMe"
